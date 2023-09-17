244e7e078cec6801d23d9aa8ffce8d59d4011c81
Ketrin06 <kateshepurna@gmail.com>
2023-09-17 15:45:15 +0300

    add html

---
+++ js/task-02.js
@@ -0,0 +1,16 @@
+const ingredients = [
+    "Potatoes",
+    "Mushrooms",
+    "Garlic",
+    "Tomatos",
+    "Herbs",
+    "Condiments",
+];
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+    const ingredientItem = document.createElement("li");
+    ingredientItem.textContent = ingredient;
+    ingredientItem.classList.add("item");
+    ingredientsList.append(ingredientItem);
+});
---
+++ js/task-04.js
@@ -0,0 +1,24 @@
+let counter = document.querySelector("#value");
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+let counterValue = 0;
+
+function updateCounter() {
+    counter.textContent = counterValue;
+}
+
+updateCounter();
+
+const incrementClick = () => {
+    counterValue++;
+    updateCounter();
+};
+
+const decrementClick = () => {
+    counterValue--;
+    updateCounter();
+};
+
+incrementBtn.addEventListener("click", incrementClick);
+decrementBtn.addEventListener("click", decrementClick);
